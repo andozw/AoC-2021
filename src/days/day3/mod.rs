b16a1b77be787d87e8a8478fced169536b5fdc2f
Sean Andersen <seana@squareup.com>
2022-01-04 21:16:09 -0800

    Day 3 with Vector. Also gross

--- src/days/day3/mod.rs
+++ src/days/day3/mod.rs
@@ -2,19 +2,23 @@ use crate::common::get_file;
 use std::io::BufRead;
 
 // Power consumption = gamma_rate * epsilon_rate.
-fn part1(filename: &str) -> i32 {
+fn part1(filename: &str) -> u32 {
     let reader = get_file(filename);
 
-    let mut count_map = [0; 12];
-    let mut entry_count = 0;
+    let mut count_map: Vec<u32> = Vec::new();
+    let mut line_count = 0;
 
     for (index, line) in reader.lines().enumerate() {
-        entry_count +=1;
-
+        line_count += 1;
         let line = line.unwrap();
-        
+
         let mut index = 0;
-        
+
+        // This is gross. Initialize the count map on the first iteration.
+        if count_map.len() == 0 {
+            count_map = line.chars().map(|c| 0).collect();
+        }
+
         // This won't work for all string type input.
         for digit in line.chars() {
             match digit {
@@ -26,22 +30,16 @@ fn part1(filename: &str) -> i32 {
             index += 1;
         }
     }
-   
+
     println!("count_map: {:?}", count_map);
 
-    let mut bitmap = [false; 12];
-    // Assume no ties.
-    // Figure out more idiomatic way to do this.
-    for (index, entry) in count_map.clone().iter().enumerate() {
-        if most_common_is_1(entry.clone(), entry_count) {
-            bitmap[index] = true;
-        } else {
-            bitmap[index] = false;
-        }
-    }
+    let bitmap: Vec<_> = count_map.iter().map(|&one_count| -> bool {
+        one_count > (line_count as u32 / 2)
+    }).collect();
+
     println!("bitmap: {:?}", bitmap);
 
-    let epsilon_bitmap = invert(bitmap.clone());
+    let epsilon_bitmap = bitmap.iter().map(|&bit| !bit).collect(); 
     let gamma_rate = convert_to_decimal(bitmap); 
     let epsilon_rate = convert_to_decimal(epsilon_bitmap); 
 
@@ -49,33 +47,24 @@ fn part1(filename: &str) -> i32 {
     gamma_rate * epsilon_rate
 }
 
-fn most_common_is_1(number: i32, entry_count: i32) -> bool {
-    number > (entry_count / 2)
+fn most_common_is_1(number: u32, line_count: u32) -> bool {
+    number > (line_count / 2)
 }
 
-fn convert_to_decimal(bitmap: [bool; 12]) -> i32 {
+fn convert_to_decimal(bitmap: Vec<bool> ) -> u32 {
     // This is so gross. I look forward to writing not clunky rust.
+    // TODO: can this be done with a reduce?
     let mut total = 0;
-    let mut pow2 = 2048;
-    for item in bitmap {
-        if item == true {
+    let mut pow2 = 1;
+    for &item in bitmap.iter().rev() {
+        if item {
             total += pow2
         }
-        
-        pow2 = pow2 >> 1;
+
+        pow2 = pow2 << 1;
     }
 
     total
-}
-
-fn invert(bitmap: [bool; 12]) -> [bool; 12] {
-    let mut inverted_bitmap = [false; 12];
-
-    for (index, entry) in bitmap.iter().enumerate() {
-        inverted_bitmap[index] = !entry;
-    }
-
-    inverted_bitmap
 }
 
 #[cfg(test)]
